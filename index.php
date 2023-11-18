7896ab13bb78bd9b463038083b8f69e6cf346cda
thomasdye12 <thomas@thomasdye.net>
2023-11-18 16:36:29 +0000

    added map

--- index.php
+++ index.php
@@ -7,79 +7,92 @@
 
 <html>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;500&display=swap" rel="stylesheet">
+
 <head>
     <title>Calendar</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-<div class="overlay"></div>
-    <div class =clock id="clock">
-        <div class =clock1 id="clock1">
+    <div class="overlay"></div>
+    <div class=clock id="clock">
+        <div class=clock1 id="clock1">
             <p class="time">{{ time }}</p>
             <p class="date">{{ date }}</p>
         </div>
     </div>
-<!-- weather info -->
-<div class="weather" id="weather">
-<div v-for="WeatherHour in WeatherData" class="weather-Hour">
-<img class="weather-icon" :src="WeatherHour.weather_icons" />
-    <p class="weather-time">{{ WeatherHour.formattedtime }} - {{ WeatherHour.temperature }}°</p>
+    <!-- weather info -->
+    <div class="weather" id="weather">
+        <div v-for="WeatherHour in WeatherData" class="weather-Hour">
+            <img class="weather-icon" :src="WeatherHour.weather_icons" />
+            <p class="weather-time">{{ WeatherHour.formattedtime }} - {{ WeatherHour.temperature }}°</p>
 
 
-</div>
-</div>
-<!-- calander info -->
-    <div id="calendar-wrapper"> 
+        </div>
+    </div>
+    <!-- calander info -->
+    <div id="calendar-wrapper">
         <!-- todays calander events only -->
-        <div class="calendar-today" id="calander-today"> 
-        <!-- <p class="title">{{ today.title }}</p> -->
-        <ul class="event-list">
-        <li class="event-item" :class="{ 'AllDay-class': event.calendar.colour === 'red' && event.isAllDay}"  v-for="(event, index) in today.events" :key="index">
-            <div class="circle" :class="getCircleColorClass(event.calendar.colour)" ></div>
-            <p class="event-text">{{ event.summary1 }}</p>
-        </li>
-    </ul>
+        <div class="calendar-today" id="calander-today">
+            <!-- <p class="title">{{ today.title }}</p> -->
+            <ul class="event-list">
+                <li class="event-item" :class="{ 'AllDay-class': event.calendar.colour === 'red' && event.isAllDay}" v-for="(event, index) in today.events" :key="index">
+                    <div class="circle" :class="getCircleColorClass(event.calendar.colour)"></div>
+                    <p class="event-text">{{ event.summary1 }}</p>
+                </li>
+            </ul>
 
+        </div>
+
+
+
+
+        <div id="calendar">
+            <div class="weekdays weekdays-text">Mon</div>
+            <div class="weekdays weekdays-text">Tue</div>
+            <div class="weekdays weekdays-text">Wed</div>
+            <div class="weekdays weekdays-text">Thu</div>
+            <div class="weekdays weekdays-text">Fri</div>
+            <div class="weekdays weekdays-text">Sat</div>
+            <div class="weekdays weekdays-text">Sun</div>
+            <div v-for="day in days" class="calendar-day">
+                <p class="title-month">{{ day.month }}</p>
+                <div class="red-circleAlways" :class="{ 'red-circle': day.isToday }">
+
+                    <p class="title">{{ day.title }}</p>
+                </div>
+                <ul class="event-list">
+                    <li class="event-item" :class="{ 'AllDay-class': event.calendar.colour === 'red' && event.isAllDay}" v-for="(event, index) in day.events" :key="index">
+                        <div class="circle" :class="getCircleColorClass(event.calendar.colour)"></div>
+                        <p class="event-text">{{ event.summary }}</p>
+                    </li>
+                    <!-- If there are more than 4 events, show a message -->
+                    <li v-if="day.events.length > 4" class="more-events">
+                        <p class="event-text">+{{ day.events.length - 4 }} more</p>
+                    </li>
+                </ul>
+
+
+            </div>
+
+        </div>
+    </div>
+    <!-- locaton map of users -->
+     <div class="mapOverview" id="map">
+     <div v-for="User in Users" class="map-Person">
+        <div class="map-PersonsMap" :id="User.Device">
+        </div>
+
+     </div>
     </div>
 
 
-
- 
-    <div id="calendar">
-    <div class="weekdays weekdays-text">Mon</div>
-        <div class="weekdays weekdays-text">Tue</div>
-        <div class="weekdays weekdays-text">Wed</div>
-        <div class="weekdays weekdays-text">Thu</div>
-        <div class="weekdays weekdays-text">Fri</div>
-        <div class="weekdays weekdays-text">Sat</div>
-        <div class="weekdays weekdays-text">Sun</div>
-    <div v-for="day in days" class="calendar-day">
-    <p class="title-month">{{ day.month }}</p>
-    <div class="red-circleAlways" :class="{ 'red-circle': day.isToday }"> 
-
-    <p class="title">{{ day.title }}</p>
-    </div>
-    <ul class="event-list">
-        <li class="event-item" :class="{ 'AllDay-class': event.calendar.colour === 'red' && event.isAllDay}"  v-for="(event, index) in day.events" :key="index">
-            <div class="circle" :class="getCircleColorClass(event.calendar.colour)" ></div>
-            <p class="event-text">{{ event.summary }}</p>
-        </li>
-        <!-- If there are more than 4 events, show a message -->
-        <li v-if="day.events.length > 4" class="more-events">
-            <p class="event-text">+{{ day.events.length - 4 }} more</p>
-        </li>
-    </ul>
-
-
-    </div>
-
-    </div>
-    </div>
-    </div>
+   
 </body>
 
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.4/vue.min.js'></script>
+<script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js"></script>
 <script type="text/javascript" src="script.js"></script>
+
 </html>
--- script.js
+++ script.js
@@ -133,7 +133,7 @@ var calendar = new Vue({
 var weather = new Vue({
     el: '#weather',
     data: {
-        WeatherData:[]
+        WeatherData: []
     },
     mounted() {
         this.getWeather();
@@ -171,5 +171,156 @@ var weather = new Vue({
 });
 
 
+// define mapkit
 
 
+var PersonMaps = new Vue({
+    el: '#map',
+    data: {
+        Users: []
+    },
+    mounted() {
+        this.getPersonMaps();
+        // Schedule your function to run every 40 minutes (40 * 60 * 1000 milliseconds) 40 * 60 * 1000
+        this.intervalId = setInterval(() => {
+            this.UpdateView();
+        }, 2 * 60 * 1000);
+
+    },
+    beforeDestroy() {
+        // Clean up the interval when the Vue instance is destroyed
+        clearInterval(this.intervalId);
+    },
+    methods: {
+        async getPersonMaps() {
+            try {
+                this.personMaps = [];
+                // Make an API request to fetch events for the given date using fetch
+                const response = await fetch(`LocationTrackingapi.php`); // Adjust the API endpoint and query parameters accordingly
+
+                if (response.ok) {
+                    const data = await response.json();
+                    // loop over the data. Appleweatherdata.forecastHourly.hours
+                    this.Users = data.location;
+                    mapkit.init({
+                        authorizationCallback: function (done) {
+                            done(data.JWT);
+                        }
+                    });
+                    // loop over the data.location
+                    // wait 2 seconds 
+                    //     data.location.forEach(element => {
+                    //         ShowMapForUser(mapkit, element.Device, element.Location.Location.latitude, element.Location.Location.longitude);
+                    //   });
+                    // wait 2 seconds before calling again
+                    const self = this;
+                    setTimeout(function () {
+                        self.Users.forEach(element => {
+                            element.map = ShowMapForUser(element, data.localisedAddress);
+                        });
+                    }, 2000);
+                } else {
+                    console.error('Error fetching personMaps:', response.status, response.statusText);
+                    return []; // Return an empty array in case of an error
+                }
+            } catch (error) {
+                console.error('Error fetching personMaps:', error);
+                return []; // Return an empty array in case of an error
+            }
+        }, async UpdateView() {
+            try {
+                this.personMaps = [];
+                // Make an API request to fetch events for the given date using fetch
+                const response = await fetch(`LocationTrackingapi.php`); // Adjust the API endpoint and query parameters accordingly
+
+                if (response.ok) {
+                    const data = await response.json();
+                    // get both the old data and the new data
+                    // loop over the data.location
+                    data.location.forEach(element => {
+                     
+                        //  get the old data
+                        var oldData = this.Users.find(x => x.Device == element.Device);
+                        if (oldData == null) {
+                            return;
+                        }
+                        // if the 2 are the same then do nothing
+                        if (oldData.Location.Location.latitude == element.Location.Location.latitude && oldData.Location.Location.longitude == element.Location.Location.longitude) {
+                            return;
+                        }
+                        // update the map
+                        UpdateMapData(oldData, element, data.localisedAddress);
+                        
+                    });
+
+                } else {
+                    console.error('Error fetching personMaps:', response.status, response.statusText);
+                    return []; // Return an empty array in case of an error
+                }
+            } catch (error) {
+                console.error('Error fetching personMaps:', error);
+                return []; // Return an empty array in case of an error
+            }
+
+        }
+
+    }
+});
+
+function ShowMapForUser(element,localisedNames) {
+    var map = new mapkit.Map(element.Device);
+    var workAnnotation = new mapkit.MarkerAnnotation(new mapkit.Coordinate(element.Location.Location.latitude, element.Location.Location.longitude));
+    workAnnotation.color = element.color;
+    workAnnotation.title = element.name;    //+ " \n "  + element.Location.formattedTime;
+    workAnnotation.subtitle = "persom";
+    workAnnotation.selected = true;
+    map.mapType = "satellite";
+    var geocoder = new mapkit.Geocoder({
+        language: "en-GB"
+    }).reverseLookup(workAnnotation.coordinate, (err, data) => {
+        workAnnotation.subtitle =LocaliseNames(data.results[0].name,localisedNames);
+        console.log(data.results[0]);
+    });
+    var newCenter = workAnnotation.coordinate;
+    var span = new mapkit.CoordinateSpan(.000001);
+    var region = new mapkit.CoordinateRegion(newCenter, span);
+    map.setRegionAnimated(region)
+
+    map.showItems([workAnnotation]);
+    return map;
+}
+
+
+function UpdateMapData(OldData, NewData,localisedNames) {
+    var map = OldData.map;
+    var workAnnotation = new mapkit.MarkerAnnotation(new mapkit.Coordinate(NewData.Location.Location.latitude, NewData.Location.Location.longitude));
+    workAnnotation.color = NewData.color;
+    workAnnotation.title = NewData.name;   //+ " \n "  + element.Location.formattedTime;
+    workAnnotation.subtitle = "";
+    workAnnotation.selected = true;
+    map.mapType = "satellite";
+    var geocoder = new mapkit.Geocoder({
+        language: "en-GB"
+    }).reverseLookup(workAnnotation.coordinate, (err, data) => {
+        workAnnotation.subtitle = LocaliseNames(data.results[0].name,localisedNames);
+        console.log(data.results[0]);
+    });
+    var newCenter = workAnnotation.coordinate;
+    var span = new mapkit.CoordinateSpan(.000001);
+    var region = new mapkit.CoordinateRegion(newCenter, span);
+    map.setRegionAnimated(region)
+
+    map.showItems([workAnnotation]);
+    return map;
+}
+
+function LocaliseNames(name,localisedNames) {
+    // if the name is one of the localised names then return the localised name
+    // else return the name
+    var localisedName = localisedNames.find(x => x.name == name);
+    if (localisedName == null) {
+        return name;
+    }
+    return localisedName.localisedName;
+
+}
