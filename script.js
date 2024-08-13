cbf87dda772c69c8d16160a7fc9092dbd8d3c7a1
thomasdye12 <thomas@thomasdye.net>
2024-08-13 13:36:18 +0100

    changes adding map

--- script.js
+++ script.js
@@ -206,178 +206,187 @@ var weather = new Vue({
 // define mapkit
 
 
-// var PersonMaps = new Vue({
-//     el: '#map',
-//     data: {
-//         Users: []
-//     },
-//     mounted() {
-//         this.getPersonMaps();
-//         // Schedule your function to run every 40 minutes (40 * 60 * 1000 milliseconds) 40 * 60 * 1000
-//         this.intervalId = setInterval(() => {
-//             this.UpdateView();
-//         }, 15 * 60 * 1000);
+var PersonMaps = new Vue({
+    el: '#map',
+    data: {
+        Users: [],
+        map: null, // Add a map object to the data
+    },
+    mounted() {
+        this.getPersonMaps();
+        this.intervalId = setInterval(() => {
+            this.UpdateView();
+        }, 15 * 60 * 1000);
+    },
+    beforeDestroy() {
+        clearInterval(this.intervalId);
+    },
+    methods: {
+        async getPersonMaps() {
+            try {
+                this.Users = [];
+                const response = await fetch(`LocationTrackingapi.php`);
 
-//     },
-//     beforeDestroy() {
-//         // Clean up the interval when the Vue instance is destroyed
-//         clearInterval(this.intervalId);
-//     },
-//     methods: {
-//         async getPersonMaps() {
-//             try {
-//                 this.personMaps = [];
-//                 // Make an API request to fetch events for the given date using fetch
-//                 const response = await fetch(`LocationTrackingapi.php`); // Adjust the API endpoint and query parameters accordingly
+                if (response.ok) {
+                    const data = await response.json();
+                    this.Users = data.location;
 
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     // loop over the data. Appleweatherdata.forecastHourly.hours
-//                     this.Users = data.location;
-//                     mapkit.init({
-//                         authorizationCallback: function (done) {
-//                             done(data.JWT);
-//                         }
-//                     });
-//                     // loop over the data.location
-//                     // wait 2 seconds 
-//                     //     data.location.forEach(element => {
-//                     //         ShowMapForUser(mapkit, element.Device, element.Location.Location.latitude, element.Location.Location.longitude);
-//                     //   });
-//                     // wait 2 seconds before calling again
-//                     const self = this;
-//                     setTimeout(function () {
-//                         self.Users.forEach(element => {
-//                             element.map = ShowMapForUser(element, data.localisedAddress);
-//                         });
-//                     }, 2000);
-//                 } else {
-//                     console.error('Error fetching personMaps:', response.status, response.statusText);
-//                     return []; // Return an empty array in case of an error
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching personMaps:', error);
-//                 return []; // Return an empty array in case of an error
-//             }
-//         }, async UpdateView() {
-//             try {
-//                 this.personMaps = [];
-//                 // Make an API request to fetch events for the given date using fetch
-//                 const response = await fetch(`LocationTrackingapi.php`); // Adjust the API endpoint and query parameters accordingly
+                    mapkit.init({
+                        authorizationCallback: function (done) {
+                            done(data.JWT);
+                        }
+                    });
 
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     // get both the old data and the new data
-//                     // loop over the data.location
-//                     data.location.forEach(element => {
-                     
-//                         //  get the old data
-//                         var oldData = this.Users.find(x => x.Device == element.Device);
-//                         if (oldData == null) {
-//                             return;
-//                         }
-//                         // if the 2 are the same then do nothing
-//                         if (oldData.Location.Location.latitude == element.Location.Location.latitude && oldData.Location.Location.longitude == element.Location.Location.longitude) {
-//                             return;
-//                         }
-//                         mapkit.init({
-//                             authorizationCallback: function (done) {
-//                                 done(data.JWT);
-//                             }
-//                         });
-//                         // update the map
-//                         UpdateMapData(oldData, element, data.localisedAddress);
-//                         // update the data
-//                         oldData.Location.Location.latitude = element.Location.Location.latitude;
-//                         oldData.Location.Location.longitude = element.Location.Location.longitude;
-                        
-//                     });
+                    // Initialize the map once
+                    this.map = new mapkit.Map("map");
+                    this.map.colorScheme = mapkit.Map.ColorSchemes.Dark;
+                    this.map.showsMapTypeControl = false;
+                    this.map.showsZoomControl = false;
+                    // Add all users to the map
+                    this.Users.forEach(element => {
+                        this.addAnnotationToMap(element, data.localisedAddress);
+                    });
 
-//                 } else {
-//                     console.error('Error fetching personMaps:', response.status, response.statusText);
-//                     return []; // Return an empty array in case of an error
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching personMaps:', error);
-//                 return []; // Return an empty array in case of an error
-//             }
+                    // Adjust the map to show all annotations
+                    this.adjustMapRegion();
 
-//         }
+                } else {
+                    console.error('Error fetching personMaps:', response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching personMaps:', error);
+            }
+        },
+        async UpdateView() {
+            try {
+                const response = await fetch(`LocationTrackingapi.php`);
 
-//     }
-// });
+                if (response.ok) {
+                    const data = await response.json();
 
-// function ShowMapForUser(element,localisedNames) {
-//     var map = new mapkit.Map(element.Device);
-//     var workAnnotation = new mapkit.MarkerAnnotation(new mapkit.Coordinate(element.Location.Location.latitude, element.Location.Location.longitude));
-//     workAnnotation.color = element.color;
-//     workAnnotation.title = element.name;    //+ " \n "  + element.Location.formattedTime;
-//     workAnnotation.subtitle = "persom";
-//     workAnnotation.selected = true;
-//     map.mapType = "satellite";
-//     var geocoder = new mapkit.Geocoder({
-//         language: "en-GB"
-//     }).reverseLookup(workAnnotation.coordinate, (err, data) => {
-//         workAnnotation.subtitle =LocaliseNames(data.results[0].name,localisedNames);
-//         console.log(data.results[0]);
-//     });
-//     if (element.imageurl) {
-//     const imageDelegate = {
-//         getImageUrl(scale, callback) {
-//             callback(element.imageurl);
-//         }
-//     };
-//     workAnnotation.glyphImage = imageDelegate;
-// }
-//     var newCenter = workAnnotation.coordinate;
-//     var span = new mapkit.CoordinateSpan(.01);
-//     var region = new mapkit.CoordinateRegion(newCenter, span);
-//     map.setRegionAnimated(region)
+                    // Loop over the new data and update the map
+                    data.location.forEach(element => {
+                        var oldData = this.Users.find(x => x.Device == element.Device);
+                        if (oldData == null) {
+                            return;
+                        }
 
-//     map.showItems([workAnnotation]);
-//     return map;
-// }
+                        if (oldData.Location.Location.latitude == element.Location.Location.latitude &&
+                            oldData.Location.Location.longitude == element.Location.Location.longitude) {
+                            return;
+                        }
 
+                        // Update the existing annotation with the new data
+                        this.updateAnnotationOnMap(oldData, element, data.localisedAddress);
 
-// function UpdateMapData(OldData, NewData,localisedNames) {
-//     var map = OldData.map;
-//     map.removeAnnotations(map.annotations);
-//     var workAnnotation = new mapkit.MarkerAnnotation(new mapkit.Coordinate(NewData.Location.Location.latitude, NewData.Location.Location.longitude));
-//     workAnnotation.color = NewData.color;
-//     workAnnotation.title = NewData.name;   //+ " \n "  + element.Location.formattedTime;
-//     workAnnotation.subtitle = "";
-//     workAnnotation.selected = true;
-//     map.mapType = "satellite";
-//     var geocoder = new mapkit.Geocoder({
-//         language: "en-GB"
-//     }).reverseLookup(workAnnotation.coordinate, (err, data) => {
-//         workAnnotation.subtitle = LocaliseNames(data.results[0].name,localisedNames);
-//         console.log(data.results[0]);
-//     });
-//     if (NewData.imageurl) {
-//         const imageDelegate = {
-//             getImageUrl(scale, callback) {
-//                 callback(NewData.imageurl);
-//             }
-//         };
-//         workAnnotation.glyphImage = imageDelegate;
-//     }
-//     var newCenter = workAnnotation.coordinate;
-//     var span = new mapkit.CoordinateSpan(.01);
-//     var region = new mapkit.CoordinateRegion(newCenter, span);
-//     map.setRegionAnimated(region)
+                        // Update the data in the Users array
+                        oldData.Location.Location.latitude = element.Location.Location.latitude;
+                        oldData.Location.Location.longitude = element.Location.Location.longitude;
+                    });
 
-//     map.showItems([workAnnotation]);
-//     return map;
-// }
+                    // Adjust the map to show all annotations
+                    this.adjustMapRegion();
 
-// function LocaliseNames(name,localisedNames) {
-//     // if the name is one of the localised names then return the localised name
-//     // else return the name
-//     var localisedName = localisedNames.find(x => x.name == name);
-//     if (localisedName == null) {
-//         return name;
-//     }
-//     return localisedName.localisedName;
+                } else {
+                    console.error('Error fetching personMaps:', response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching personMaps:', error);
+            }
+        },
+        addAnnotationToMap(element, localisedNames) {
+            var annotation = new mapkit.MarkerAnnotation(
+                new mapkit.Coordinate(element.Location.Location.latitude, element.Location.Location.longitude)
+            );
+            annotation.color = element.color;
+            annotation.title = element.name;
+           
+            annotation.subtitle = "person";
+            // annotation.displayPriority = mapkit.AnnotationDisplayPriority.High;
+            // annotation.selected = true;
 
-// }
+            var geocoder = new mapkit.Geocoder({
+                language: "en-GB"
+            }).reverseLookup(annotation.coordinate, (err, data) => {
+                annotation.subtitle = LocaliseNames(data.results[0].name, localisedNames);
+            });
+
+            if (element.imageurl) {
+                const imageDelegate = {
+                    getImageUrl(scale, callback) {
+                        callback(element.imageurl);
+                    }
+                };
+                annotation.glyphImage = imageDelegate;
+            }
+
+            annotation.titleVisibility = mapkit.FeatureVisibility.Visible;
+            annotation.subtitleVisibility = mapkit.FeatureVisibility.Visible;
+            // Add the annotation to the map
+            this.map.addAnnotation(annotation);
+            
+
+            // Store the annotation in the user's data for future updates
+            element.annotation = annotation;
+        },
+        updateAnnotationOnMap(oldData, newData, localisedNames) {
+            var annotation = oldData.annotation;
+
+            // Update the annotation's coordinate
+            annotation.coordinate = new mapkit.Coordinate(newData.Location.Location.latitude, newData.Location.Location.longitude);
+            annotation.color = newData.color;
+            annotation.title = newData.name;
+
+            var geocoder = new mapkit.Geocoder({
+                language: "en-GB"
+            }).reverseLookup(annotation.coordinate, (err, data) => {
+                annotation.subtitle = LocaliseNames(data.results[0].name, localisedNames);
+            });
+
+            if (newData.imageurl) {
+                const imageDelegate = {
+                    getImageUrl(scale, callback) {
+                        callback(newData.imageurl);
+                    }
+                };
+                annotation.glyphImage = imageDelegate;
+            }
+
+            annotation.titleVisibility = mapkit.FeatureVisibility.Visible;
+            annotation.subtitleVisibility = mapkit.FeatureVisibility.Visible;
+            // Update the map region to center around the new location
+        },
+        adjustMapRegion() {
+            if (this.Users.length === 0) return;
+
+            let minLat = Infinity, maxLat = -Infinity, minLon = Infinity, maxLon = -Infinity;
+
+            this.Users.forEach(user => {
+                const lat = user.Location.Location.latitude;
+                const lon = user.Location.Location.longitude;
+
+                if (lat < minLat) minLat = lat;
+                if (lat > maxLat) maxLat = lat;
+                if (lon < minLon) minLon = lon;
+                if (lon > maxLon) maxLon = lon;
+            });
+
+            const centerLat = (minLat + maxLat) / 2;
+            const centerLon = (minLon + maxLon) / 2;
+            const spanLat = maxLat - minLat;
+            const spanLon = maxLon - minLon;
+
+            const region = new mapkit.CoordinateRegion(
+                new mapkit.Coordinate(centerLat, centerLon),
+                new mapkit.CoordinateSpan(spanLat * 1.1, spanLon * 1.1) // Adding some padding
+            );
+
+            this.map.setRegionAnimated(region);
+        }
+    }
+});
+
+function LocaliseNames(name, localisedNames) {
+    var localisedName = localisedNames.find(x => x.name == name);
+    return localisedName ? localisedName.localisedName : name;
+}
